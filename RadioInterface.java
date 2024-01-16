05510f06219919a80290d5253d56a6836fc252d3
jjaquezito <123708276+jaq23369@users.noreply.github.com>
2024-01-15 21:03:48 -0600

    RadioInterface
    Creación de la interfaz para que funcione el radio.

---
+++ RadioInterface.java
@@ -0,0 +1,9 @@
+public interface RadioInterface {
+    void encender();
+    void apagar();
+    int getVolumen();
+    void setVolumen(int volumen);
+    void cambiarBanda();
+    void subirEmisora();
+    void bajarEmisora();
+}
