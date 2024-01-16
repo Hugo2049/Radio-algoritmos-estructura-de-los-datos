12d8c2d73bf626670de06700895b8259358d99e7
Hb_W <hugobarillasvi@hotmail.com>
2024-01-15 21:18:49 -0600

    crear CarRadio que es la clase radio

---
+++ CarRadio.java
@@ -0,0 +1,89 @@
+public class CarRadio implements RadioInterface {
+    private boolean encendido;
+    private int volumen;
+    private double emisoraAM;
+    private double emisoraFM;
+
+    public CarRadio() {
+        this.encendido = false;
+        this.volumen = 0;
+        this.emisoraAM = 530; // Valor inicial para AM
+        this.emisoraFM = 87.9; // Valor inicial para FM
+    }
+
+    @Override
+    public void encender() {
+        encendido = true;
+        // Lógica adicional si es necesario
+    }
+
+    @Override
+    public void apagar() {
+        encendido = false;
+        // Lógica adicional si es necesario
+    }
+
+    @Override
+    public int getVolumen() {
+        return volumen;
+    }
+
+    @Override
+    public void setVolumen(int volumen) {
+        if (encendido) {
+            this.volumen = volumen;
+            // Lógica adicional si es necesario
+        }
+    }
+
+    @Override
+    public void cambiarBanda() {
+        if (encendido) {
+            if (emisoraAM <= 1610) {
+                emisoraAM += 10;
+            } else {
+                emisoraAM = 530;
+            }
+            if (emisoraFM <= 107.9) {
+                emisoraFM += 0.2;
+            } else {
+                emisoraFM = 87.9;
+            }
+            // Lógica adicional si es necesario
+        }
+    }
+
+    @Override
+    public void subirEmisora() {
+        if (encendido) {
+            if (emisoraAM < 1610) {
+                emisoraAM += 10;
+            } else {
+                emisoraAM = 530;
+            }
+            if (emisoraFM < 107.9) {
+                emisoraFM += 0.2;
+            } else {
+                emisoraFM = 87.9;
+            }
+            // Lógica adicional si es necesario
+        }
+    }
+
+    @Override
+    public void bajarEmisora() {
+        if (encendido) {
+            if (emisoraAM > 530) {
+                emisoraAM -= 10;
+            } else {
+                emisoraAM = 1610;
+            }
+            if (emisoraFM > 87.9) {
+                emisoraFM -= 0.2;
+            } else {
+                emisoraFM = 107.9;
+            }
+            // Lógica adicional si es necesario
+        }
+    }
+}
